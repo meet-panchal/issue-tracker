8717892a0b10c7999604404467a1d6f9329cafc4
meet-panchal <connect.meetpanchal@gmail.com>
2019-07-22 15:50:56 +0530

    Done with all the JS

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue)
+
+function saveIssue() {
+    var issueDesc = document.getElementById('issueDescInput').value;
+    var issueSeverity = document.getElementById('issueSeverityInput').value;
+    var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+    var issueId = chance.guid();
+    var issueStatus = 'Open'
+
+    var issue = {
+        id: issueId,
+        description: issueDesc,
+        severity: issueSeverity,
+        assignedTo: issueAssignedTo,
+        status: issueStatus
+    }
+    if (localStorage.getItem('issue') == null) {
+        var issues = []
+        issues.push(issue)
+        localStorage.setItem('issues', JSON.stringify(issues))
+    } else {
+        var issues = JSON.parse(localStorage.getItem('issues'))
+        issues.push(issue)
+        localStorage.setItem('issues', JSON.stringify(issues))
+    }
+    document.getElementById(issueInputForm).reset()
+    fetchIssues()
+    e.preventDefault()
+
+}
+
 function fetchIssues(issue) {
     var issues = JSON.parse(localStorage.getItem('issues'))
     var issuesList = document.getElementById('issuesList')
@@ -7,11 +38,15 @@ function fetchIssues(issue) {
         var desc = issues[i].description;
         var severity = issues[i].assignedTo;
         var status = issues[i].status;
-
-        issuesList.innerHTML += `<div class = "well">
+        issuesList.innerHTML +=
+            `<div class = "well">
       <h6>Issue ID : ${id} </h6>
       <p><span class = "label label-info ">${status}</span></p>
-      <h3></h3>
-      `
+      <h3>${desc}</h3>
+      <p><span class = "glyphicon glyphicon-time"></span>${severity}</p>
+      <p><span class = "glyphicon glyphicon-user"></span>${assignedTo}</p>
+      <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
+      <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
+    </div>`;
     }
 }
